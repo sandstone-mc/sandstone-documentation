52b794fb1be6c20ae104c957c38b5c6711119500
Ian Senne <IanSSenne@gmail.com>
2023-12-01 17:15:44 -0800

    cleanup & allow editing imports

--- src/utils/compiler.ts
+++ src/utils/compiler.ts
@@ -1,24 +1,21 @@
-let lib: Promise<typeof import("@sandstone-mc/playground")>;
+let lib: Promise<typeof import("@sandstone-mc/playground")> =
+  "window" in globalThis ? import("@sandstone-mc/playground") : null;
 export type CustomHandlerFileObject =
   | { key: number; relativePath: string; content: string }
   | { type: "errors"; relativePath: string; content: string; key: number };
-let isSafeToCompile = true;
+
 export async function compileDataPack(
   tsCode: string
 ): Promise<{ result: Record<string, string> }> {
   if (!lib) {
-    isSafeToCompile = false;
     lib = import("@sandstone-mc/playground");
   }
+
   const { compilePack } = await lib;
   const result = await compilePack({
     "/index.ts": tsCode,
   });
 
-  console.log({
-    tsCode,
-    result,
-  });
   if (result.success === false) {
     throw new Error(result.error);
   }
